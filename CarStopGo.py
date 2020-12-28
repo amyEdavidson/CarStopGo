360f5e0155a1e87bf75595ab00538749d759195f
Amy Davidson <73243375+amyEdavidson@users.noreply.github.com>
2020-12-28 10:43:34 -0500

    Add files via upload

---
+++ CarStopGo.py
@@ -0,0 +1,29 @@
+print("what colour is the traffic light?")
+lightColour = input()
+
+print("what is the distance in meters to the intersection?")
+distance = input()
+distance = float(distance)
+
+print("what is the speed of the car in meters")
+speed = input()
+speed = float(speed)
+
+if lightColour == "green":
+    print("Go")
+
+if lightColour == "yellow":
+    if speed <= 5:
+        print("Go")
+    else:
+        print("Stop")
+
+if lightColour == "red":
+    if speed <= 2:
+        print("Go")
+    else:
+        print("Stop")
+
+if lightColour != "green" | "yellow" | "red":
+    print("Stop") 
+
